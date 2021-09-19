1952138c6d44a57693dae6e19be27d039cd97c8b
Sergey <levashov04sergey@gmail.com>
2021-09-19 16:51:10 +0300

    cloning app,  bind front & back, using cors, apollo-boost, react-apollo

--- server/server/app.js
+++ server/server/app.js
@@ -2,16 +2,20 @@ const express = require('express');
 const {graphqlHTTP} = require('express-graphql');
 const schema = require('../schema/schema');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const app = express();
 const PORT = 3005;
 
 mongoose.connect('mongodb+srv://StTrace:1234@cluster0.gwy0v.mongodb.net/graphql-tutorial?retryWrites=true&w=majority', {useUnifiedTopology: true, useNewUrlParser: true});
 
+app.use(cors());
+
 app.use('/graphql', graphqlHTTP({
     schema,
     graphiql: true,
 }));
+
 
 const dbConnection = mongoose.connection;
 dbConnection.on('error', err => console.log(`Connection error: ${err}`));
