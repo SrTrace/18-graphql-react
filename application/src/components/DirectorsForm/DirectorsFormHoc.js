e6065b2e109f9d3afdef6e67825afcd928468185
Sergey <levashov04sergey@gmail.com>
2021-09-20 20:45:52 +0300

    editing db elements from ui, mutation update

--- application/src/components/DirectorsForm/DirectorsFormHoc.js
+++ application/src/components/DirectorsForm/DirectorsFormHoc.js
@@ -2,18 +2,29 @@ import { withStyles } from '@material-ui/core/styles';
 import { compose } from 'recompose';
 import {graphql} from "react-apollo";
 
-import {addDirectorMutation} from "./mutation";
+import {addDirectorMutation, updatedDirectorMutation} from "./mutation";
 import {directorsQuery} from "../DirectorsTable/queries";
 
 import { styles } from './styles';
 
-const withGraphqlAdd = graphql(addDirectorMutation, {
-   props: ({mutate}) =>  ({
-       addDirector: director => mutate({
-           variables: director,
-           refetchQueries: [{query: directorsQuery}],
-       }),
-   }),
-});
+const withGraphQl = compose(
+    graphql(addDirectorMutation, {
+        props: ({mutate}) =>  ({
+            addDirector: director => mutate({
+                variables: director,
+                refetchQueries: [{query: directorsQuery}],
+            }),
+        }),
+    }),
+    graphql(updatedDirectorMutation, {
+        props: ({mutate}) =>  ({
+            updateDirector: director => mutate({
+                variables: director,
+                refetchQueries: [{query: directorsQuery}],
+            }),
+        }),
+    })
+);
 
-export default compose(withStyles(styles), withGraphqlAdd);
+
+export default compose(withStyles(styles), withGraphQl);
